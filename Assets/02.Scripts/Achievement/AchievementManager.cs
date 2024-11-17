946559225c1f1452d5421c4b4adf8c9e5a371524
lostwaltz <lostwaltz89@gmail.com>
2024-11-17 14:16:33 +0900

    Update AchManager

--- Assets/02.Scripts/Achievement/AchievementManager.cs
+++ Assets/02.Scripts/Achievement/AchievementManager.cs
@@ -28,7 +28,7 @@ namespace Achievement
             if (Input.GetKeyDown(KeyCode.Space))
                 _eventManager.Publish(EventManager.Channel.Achievement, new EventAchievement(Achievement.Action.Kill, Achievement.Target.Monster, 1f));
             if (Input.GetKeyDown(KeyCode.Escape))
-                _eventManager.Publish(EventManager.Channel.Achievement, new EventAchievement(Achievement.Action.Kill, Achievement.Target.Cash, 1f, 1000));
+                _eventManager.Publish(EventManager.Channel.Achievement, new EventAchievement(Achievement.Action.Kill, Achievement.Target.Cash, 1f, 100));
         }
 
         private void OnAchievement(EventAchievement data)
