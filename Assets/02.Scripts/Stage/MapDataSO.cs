2252a70e565be0ce6a9281da1101f92e536b793a
SONGMYEONGSEONG <100818983+SONGMYEONGSEONG@users.noreply.github.com>
2024-11-17 14:20:39 +0900

    Update : Font 설정

--- Assets/02.Scripts/Stage/MapDataSO.cs
+++ Assets/02.Scripts/Stage/MapDataSO.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 [System.Serializable]
 public struct RowLine
 {
-    public int[] index;
+    public int[] index; // 피드백 : int 데이터를 enum으로 변경해서 사용할것 
 }
 
 [CreateAssetMenu(fileName = "MapDataSO", menuName = "MapDataSO/Default", order = 0)]
 public class MapDataSO : ScriptableObject
 {
+
     public int Row;//열 , 세로
     public int Column;//행 ,가로
 
-    public RowLine[] MapMatrix;
+    public RowLine[] MapMatrix; // 피드백 : int 데이터를 enum으로 변경해서 사용할것 
 
     public List<Vector3> WayPoint; // 나중에는 Queue로 작성하여 동적생성 할수 있게 사용할거
     public List<Vector3> PlayerTilePoint; // 플레이어가 배치할 수 있는 타일 
