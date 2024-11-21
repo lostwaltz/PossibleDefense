a9aa1e593a638a9479c1588c1c46d7da552276ee
devH <146154052+0umj@users.noreply.github.com>
2024-11-21 22:36:57 +0900

    fix: 타워 범위 ui 표시 안 되는 오류 수정
    StageManger에서  TowerController Init 메서드 호출을 통해 해결

--- Assets/02.Scripts/Enemy/ParticleControl/FollowParticle.cs
+++ Assets/02.Scripts/Enemy/ParticleControl/FollowParticle.cs
@@ -6,13 +6,7 @@ public class FollowParticle : BaseParticle
 {
     private Transform Target;
     private Vector3 offset = new Vector3(0, 0.1f, 0);
-
-
-    protected void Awake()
-    {
-        base.Awake();
-    }
-
+    
     protected override void OnParticleUpdate()
     {
         base.OnParticleUpdate();
