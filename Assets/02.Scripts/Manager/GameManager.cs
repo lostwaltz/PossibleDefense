c4008212e8bcdfd9986f559827351c5e54dbd779
SONGMYEONGSEONG <100818983+SONGMYEONGSEONG@users.noreply.github.com>
2024-11-21 20:28:09 +0900

    Update : GameManager ClearStage 추가

--- Assets/02.Scripts/Manager/GameManager.cs
+++ Assets/02.Scripts/Manager/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class GameManager : SingletonDontDestroy<GameManager>
 {
     public int CallStageNum; // 스테이지 넘버
+    public int curClearStageNum = 0; // 스테이지 넘버
     public int PlayerGold; // 로비에서 사용되는 플레이어 골드
 
     protected override void Awake()
