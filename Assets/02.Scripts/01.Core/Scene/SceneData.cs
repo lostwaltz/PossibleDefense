ffe86f4c094c6be18b101789c4e98e1cb4e666cb
SONGMYEONGSEONG <100818983+SONGMYEONGSEONG@users.noreply.github.com>
2024-11-22 10:22:13 +0900

    Update : Build 준비 작업

---
+++ Assets/02.Scripts/01.Core/Scene/SceneData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class SceneData
+{
+    public SceneAsset sceneAsset;
+
+    public void Test()
+    {
+        SceneManager.LoadScene(sceneAsset.name);
+    }
+}
